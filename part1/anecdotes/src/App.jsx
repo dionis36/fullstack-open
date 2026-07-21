b7389adbedc8cd8b89e9d78cb7326c7a50c33b66
Dionis Nasuwa <nasuwadio36@gmail.com>
2026-07-21 22:32:55 +0300

    part1: complete anecdotes (1.12-1.14) - Part 1 finished

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.',
+    'The best error message is the one that never shows up.',
+    'It’s not a bug – it’s an undocumented feature.',
+    'First, solve the problem. Then, write the code.',
+    'There are only two hard things in Computer Science: cache invalidation and naming things.',
+    'Before software can be reusable it first has to be usable.',
+    'Make it work, make it right, make it fast.'
+  ]
+
+  const [selected, setSelected] = useState(0)
+
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
+
+  const handleNextAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomIndex)
+  }
+
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected] += 1
+    setVotes(copy)
+  }
+
+  const maxVotes = Math.max(...votes)
+  const mostPopularIndex = votes.indexOf(maxVotes)
+
+  return (
+    <div>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+      <button onClick={handleVote}>vote</button>
+      <button onClick={handleNextAnecdote}>next anecdote</button>
+
+      <h1>Anecdote with most votes</h1>
+      {maxVotes === 0 ? (
+        <p>No votes registered yet</p>
+      ) : (
+        <div>
+          <p>{anecdotes[mostPopularIndex]}</p>
+          <p>has {maxVotes} votes</p>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default App
